749537a9dec98851201b744add1b5764beac3db0
ABHIJITH <aabhivishnu@gmail.com>
2024-03-03 00:03:04 +0530

    Initial commit

---
+++ EVENT_RABBITER_CONFIG_SERVICE_ONE/src/main/java/com/EVENT_RABBITER_CONFIG_SERVICE_ONE/EVENT_RABBITER_CONFIG_SERVICE_ONE/EventRabbiterConfigServiceOneApplication.java
@@ -0,0 +1,13 @@
+package com.EVENT_RABBITER_CONFIG_SERVICE_ONE.EVENT_RABBITER_CONFIG_SERVICE_ONE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventRabbiterConfigServiceOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventRabbiterConfigServiceOneApplication.class, args);
+	}
+
+}
---
+++ EVENT_RABBITER_CONFIG_SERVICE_ONE/src/test/java/com/EVENT_RABBITER_CONFIG_SERVICE_ONE/EVENT_RABBITER_CONFIG_SERVICE_ONE/EventRabbiterConfigServiceOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EVENT_RABBITER_CONFIG_SERVICE_ONE.EVENT_RABBITER_CONFIG_SERVICE_ONE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventRabbiterConfigServiceOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
